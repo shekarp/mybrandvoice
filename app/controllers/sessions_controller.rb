9c1c4dfcc5e3c55b8a885a8fa671506fd6e053f1
Ralf Gueldemeister <ralf.gueldemeister@sv.cmu.edu>
2012-03-18 14:16:35 -0700

    fixed redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def new
     authenticate Auth::Facebook.authenticate(cookies)
-    redirect_to consumers_url
+    redirect_to '/consumers/1'
   end
 end
